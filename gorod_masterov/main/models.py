785e94b6e10f31e68d2a9c5c30daae09bc793628
andrey2kulagin <andrey2kulagin@yandex.ru>
2022-06-25 14:37:01 +0300

    Make small site

--- gorod_masterov/main/models.py
+++ gorod_masterov/main/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+class Task(models.Model):
+    number = models.CharField("Phone number", max_length=15)
+    name = models.CharField("Name", max_length=50)
+    def __str__(self):
+        return self.title
